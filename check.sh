a11cc976067ea0dd2f057a7205b1c00b9f14b164
idk <problemsolver@openmailbox.org>
2017-05-03 17:21:21 -0400

    github alias will either search or open the github repo in the current directory

--- check.sh
+++ check.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ./tests
 
-shellcheck envsetup.sh "tests/envsetup.sh"
+shellcheck envsetup.sh | tee "tests/envsetup.sh"
 
 for file in ./*aliases; do
         shellcheck "$file" | tee "tests/$file.test"
