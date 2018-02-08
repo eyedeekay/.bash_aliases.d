2b1adf6d12de8c8a60303818a71fceb521ceb345
idk <problemsolver@openmailbox.org>
2018-02-07 21:37:16 -0500

    revised envsetup.

--- envsetup.sh
+++ envsetup.sh
@@ -2,25 +2,16 @@
 
 if [ -f /usr/lib/fireaxe-aliases/aliases_settings ]; then
     . /usr/lib/fireaxe-aliases/aliases_settings
+    for file in /usr/lib/fireaxe-aliases/bash_aliases_*; do
+        echo "loading aliases file: $file"
+        . "$file"
+    done
 fi
 
-if [ -f "$HOME/.bash_aliases/aliases_settings" ]; then
-    . "$HOME/.bash_aliases/aliases_settings"
-fi
-
-for file in /usr/lib/fireaxe-aliases/bash_aliases_*; do
-    echo "loading aliases file: $file"
-    . "$file"
-done
-
-for file in $HOME/.bash_aliases.d/bash_aliases_*; do
-    echo "loading aliases file: $file"
-    . "$file"
-done
-
-reload_aliases(){
+if [ -f "$HOME/.bash_aliases.d/aliases_settings" ]; then
+    . "$HOME/.bash_aliases.d/aliases_settings"
     for file in $HOME/.bash_aliases.d/bash_aliases_*; do
         echo "loading aliases file: $file"
         . "$file"
     done
-}
+fi
